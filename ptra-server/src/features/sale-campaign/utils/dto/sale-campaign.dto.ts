e93aba75fcfa299b00608f739f12173cc7208e6c
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-02-09 16:04:30 +0700

    feat: PTRA-0 campaign dto

--- ptra-server/src/features/sale-campaign/utils/dto/sale-campaign.dto.ts
+++ ptra-server/src/features/sale-campaign/utils/dto/sale-campaign.dto.ts
@@ -1,7 +1,9 @@
 import { IsNotEmpty } from 'class-validator';
 import ProductDto from './product.dto';
+import CampaignDto from './campaign.dto';
 
 export default class SaleCampaignDto {
   @IsNotEmpty()
   products: [ProductDto];
+  campaigns?: [CampaignDto];
 }
