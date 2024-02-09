ea07c6d967ba47b971e488d3cf9a8e257f49b353
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-02-09 18:40:38 +0700

    refactor: PTRA-0 calculation strategy

--- ptra-server/src/features/sale-campaign/sale-campaign.service.ts
+++ ptra-server/src/features/sale-campaign/sale-campaign.service.ts
@@ -1,9 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import SaleCampaignDto from './utils/dto/sale-campaign.dto';
 import TotalResultEntity from './utils/entities/total-price-result.entity';
-import ProductDto from './utils/dto/product.dto';
 import { IDatabaseService } from 'src/utils/database/database.service';
-import SaleCampaignMessageConstant from './utils/constants/sale-campaign-message.constant';
+import { NoCampaignStrategy } from './utils/strategies/calculation.strategy';
 
 @Injectable()
 export class SaleCampaignService {
@@ -13,29 +12,14 @@ export class SaleCampaignService {
     // Case null or empty campaigns
     if (!dto.campaigns) {
       return {
-        totalPrice: this._calculatePriceWithoutCampaign(dto.products),
+        totalPrice: new NoCampaignStrategy(this.databaseService).execute(
+          dto.products,
+        ),
       };
     }
 
     return {
       totalPrice: 0,
     };
-  }
-
-  private _calculatePriceWithoutCampaign(products: [ProductDto]): number {
-    let result = 0;
-
-    products.forEach((e) => {
-      const product = this.databaseService.getProductById(e.id);
-      if (product) {
-        result += product.price * e.quantity;
-        return;
-      }
-      throw new BadRequestException({
-        message: SaleCampaignMessageConstant.productNotFound,
-      });
-    });
-
-    return result;
   }
 }
