343483e9be21c0ac544a0ffb9c6c5311c6aa93d5
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-02-09 23:47:27 +0700

    feat: PTRA-0 apply pipe on controller

--- ptra-server/src/features/sale-campaign/sale-campaign.controller.ts
+++ ptra-server/src/features/sale-campaign/sale-campaign.controller.ts
@@ -2,14 +2,22 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import TotalResultEntity from './utils/entities/total-price-result.entity';
 import { SaleCampaignService } from './sale-campaign.service';
 import SaleCampaignDto from './utils/dto/sale-campaign.dto';
+import { IDatabaseService } from '../../utils/database/database.service';
+import SaleCampaignValidationPipe from './utils/pipes/sale-campaign-validation.pipe';
 
 @Controller('api/v1/sale-campaign')
 export class SaleCampaignController {
-  constructor(private readonly saleCampaignService: SaleCampaignService) {}
+  constructor(
+    private readonly saleCampaignService: SaleCampaignService,
+    private readonly databaseService: IDatabaseService,
+  ) {}
 
   @Post()
   @HttpCode(HttpStatus.OK)
   getFinalTotalPrice(@Body() dto: SaleCampaignDto): TotalResultEntity {
+    const pipe = new SaleCampaignValidationPipe(this.databaseService);
+    pipe.transform(dto, { type: 'body', metatype: SaleCampaignDto });
+
     const result = this.saleCampaignService.getFinalPriceTotalPrice(dto);
     return result;
   }
