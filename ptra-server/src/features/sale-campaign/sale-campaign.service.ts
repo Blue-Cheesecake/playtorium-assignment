d34d59a212bf9358a2ceed1e2354889dc1016d5e
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-02-12 11:52:19 +0700

    Resolve PTRA-0 "Feature/ percentage discount"
    * feat: PTRA-0 percentage discount strategy
    
    * test: PTRA-0 percentage discount
    
    * test: PTRA-0 e2e percentage discount

--- ptra-server/src/features/sale-campaign/sale-campaign.service.ts
+++ ptra-server/src/features/sale-campaign/sale-campaign.service.ts
@@ -1,11 +1,14 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import SaleCampaignDto from './utils/dto/sale-campaign.dto';
 import TotalResultEntity from './utils/entities/total-price-result.entity';
-import { IDatabaseService } from 'src/utils/database/database.service';
+import { IDatabaseService } from '../..//utils/database/database.service';
 import ProductDto from './utils/dto/product.dto';
 import SaleCampaignMessageConstant from './utils/constants/sale-campaign-message.constant';
 import { CampaignType } from '../../utils/enum/campaign-type.enum';
-import { FixedAmountDiscountStrategy } from './utils/strategies/campaign-discount.strategy';
+import {
+  FixedAmountDiscountStrategy,
+  PercentageDiscountStrategy,
+} from './utils/strategies/campaign-discount.strategy';
 
 @Injectable()
 export class SaleCampaignService {
@@ -17,19 +20,25 @@ export class SaleCampaignService {
     if (dto.campaigns) {
       for (const campaignDto of dto.campaigns) {
         const campaign = this.databaseService.getCampaignById(campaignDto.id);
-
         switch (campaign.type) {
           case CampaignType.fixedAmount:
-            const strategy = new FixedAmountDiscountStrategy();
-            result = strategy.getDiscount(result, campaignDto.discount);
+            result = new FixedAmountDiscountStrategy().getDiscount(
+              result,
+              campaignDto.discount,
+            );
             break;
           case CampaignType.percentageDiscount:
+            result = new PercentageDiscountStrategy().getDiscount(
+              result,
+              campaignDto.discount,
+            );
             break;
           case CampaignType.percentageDiscountByItemCategory:
             break;
           case CampaignType.discountByPoint:
             break;
           case CampaignType.specialCampaign:
+            break;
         }
       }
     }
