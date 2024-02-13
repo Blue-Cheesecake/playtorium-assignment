b44056fe2547c114d33b629fb95fa33c737c0816
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-02-13 11:58:40 +0700

    feat: PTRA-0 product cart ui

--- ptra-mobile/lib/features/home/pages/cart/cart_page.dart
+++ ptra-mobile/lib/features/home/pages/cart/cart_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CartPage extends StatelessWidget {
+import 'data/data.dart';
+import 'logic/logic.dart';
+import 'widgets/widgets.dart';
+
+class CartPage extends ConsumerWidget {
   const CartPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productsMap = ref.watch(cartInputStateProvider.select((value) => value.products));
+    final products = <CartInputInfoModel>[];
+    productsMap.forEach((_, v) {
+      products.add(v);
+    });
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart'),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: productsMap.length,
+            padding: const EdgeInsets.all(10),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: CartItemWD(info: products[index]),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
